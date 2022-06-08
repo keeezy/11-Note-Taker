86b1ff079c07aa5bfae9a12e27d31ec656eb4d93
keeezy <lazarokev@gmail.com>
2022-06-07 18:35:01 -0700

    fixed directory issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,24 +3,24 @@ const path = require("path")
 const fs = require("fs")
 const uuid = require("../helpers/uuid");
 
-router.get("api/notes", (req, res) => {
-    const notes = fs.readFileSync(path.join(process.cwd(), "/db/db.json"));
+router.get("/api/notes", (req, res) => {
+    const notes = fs.readFileSync(path.join(process.cwd(), "db/db.json"));
     const parsedNotes = JSON.parse(notes);
     res.json(parsedNotes);
 })
 
 
 
-router.post("api/notes", (req, res) => {
+router.post("/api/notes", (req, res) => {
     // reads file
-    const currentSaves = fs.readFileSync(path.join(process.cwd(), "/db/db.json"));
+    const currentSaves = fs.readFileSync(path.join(process.cwd(), "db/db.json"));
     // parses current saves into JSON
     const parsedSaves = JSON.parse(currentSaves);
     // Adds new note
     const newSaves = [...parsedSaves, { title: req.body.title, text: req.body.text, id: uuid()}];
 
     // writes file
-    fs.writeFileSync(path.join(process.cwd(), "/db/db.json"), JSON.stringify(newSaves), res.json(newSaves));
+    fs.writeFileSync(path.join(process.cwd(), "db/db.json"), JSON.stringify(newSaves), res.json(newSaves));
 });
 
 module.exports = router;
